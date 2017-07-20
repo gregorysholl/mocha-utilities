324f69c7d03ef1e542a2570527eb15a27ef1bb3d
Gregory Sholl e Santos <gregorysholl@gmail.com>
2017-07-19 21:18:52 -0300

    Add path methods for documents.

--- MochaUtilities/Classes/Core/Utils/DocumentsUtil.swift
+++ MochaUtilities/Classes/Core/Utils/DocumentsUtil.swift
@@ -8,7 +8,30 @@
 
 import UIKit
 
+//MARK: - Variables & Accessors
+
 public class DocumentsUtil {
     
     private init() {}
+}
+
+//MARK: - Path
+
+public extension DocumentsUtil {
+    
+    public func path(forDomainMask domainMask: FileManager.SearchPathDomainMask = .userDomainMask) throws -> String {
+        let documentPaths = NSSearchPathForDirectoriesInDomains(.documentDirectory, domainMask, true)
+        if documentPaths.isEmpty {
+            throw MochaException.fileNotFoundException
+        }
+        return documentPaths[0]
+    }
+    
+    public func path(of filename: String?, with domainMask: FileManager.SearchPathDomainMask = .userDomainMask) throws -> String {
+        guard let filename = filename, filename.isNotEmpty else {
+            throw MochaException.fileNotFoundException
+        }
+        let documentPath = try path(forDomainMask: domainMask)
+        return documentPath.appendingPathComponent(filename)
+    }
 }
